1aa22e33e5f19e0ddceb126a24778d68244d942c
Shtogrin <ashtogrin@taker.io>
2023-08-22 16:05:23 +0300

    first commit

--- selenium_course/lessons/lesson2.2_step8.py
+++ selenium_course/lessons/lesson2.2_step8.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://suninjuly.github.io/file_input.html"
 current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла
-file_path = os.path.join(current_dir, 'text.txt')           # добавляем к этому пути имя файла
+file_path = os.path.join(current_dir, '../text.txt')           # добавляем к этому пути имя файла
 
 try:
 
